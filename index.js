8087d84aac008ee8de2f2050c2a5bddc1fa3e822
Glenn Rempe <glenn@rempe.us>
2016-03-17 14:36:21 -0700

    Minor stylistic cleanup. http://standardjs.com compliant

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ nacl.util = require('tweetnacl-util')
 
 // Input: User key hash (Uint8Array), Salt (Uint8Array), callback function
 // Result: Calls scrypt which returns
-//	32 bytes of key material in a Uint8Array,
-//	which then passed to the callback.
-function getScryptKey(key, salt, callback) {
-	scrypt(key, salt, 17, 8, 32, 1000, function(keyBytes) {
-		return callback(nacl.util.decodeBase64(keyBytes))
-	}, 'base64');
+//    32 bytes of key material in a Uint8Array,
+//    which is then passed to the callback.
+function getScryptKey (key, salt, callback) {
+  scrypt(key, salt, 17, 8, 32, 1000, function (keyBytes) {
+    return callback(nacl.util.decodeBase64(keyBytes))
+  }, 'base64')
 }
 
-module.exports = function session(email, password, callback) {
+module.exports = function session (email, password, callback) {
   var keyHash = new BLAKE2s(nacl.box.secretKeyLength)
 
   keyHash.update(nacl.util.decodeUTF8(password))
@@ -24,7 +24,7 @@ module.exports = function session(email, password, callback) {
   var key = keyHash.digest()
   var salt = nacl.util.decodeUTF8(email)
 
-  getScryptKey(key, salt, function(keyBytes) {
+  getScryptKey(key, salt, function (keyBytes) {
     try {
       var keyPair = nacl.box.keyPair.fromSecretKey(keyBytes)
       callback(null, keyPair)
--- test.js
+++ test.js
@@ -4,15 +4,15 @@ nacl.util = require('tweetnacl-util')
 
 var session25519 = require('./')
 
-test('basic', function(t) {
-  session25519('user@example.com', 'secret', function(err, key) {
+test('basic', function (t) {
+  session25519('user@example.com', 'secret', function (err, key) {
     t.notOk(err, 'no error')
-    
+
     t.deepEqual(key, {
       publicKey: nacl.util.decodeBase64('EHfJ2sJJt7LjamnuJul6TlCb5SkgAwJI1PP7q95jh04='),
       secretKey: nacl.util.decodeBase64('p6FU22u0423AcS0AKY6I+XeBTMDzQLX57XOH9Xg28pw=')
     }, 'correct key has been calculated')
-    
+
     t.end()
   })
 })
