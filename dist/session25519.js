3564f61c0a851c9a3313c555f020fd71d16ae039
Glenn Rempe <glenn@rempe.us>
2016-03-17 22:49:47 -0700

    Rebuild dist/session25519.js

--- dist/session25519.js
+++ dist/session25519.js
@@ -1,6 +1,50 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var BLAKE2s = require('blake2s-js')
+var scrypt = require('scrypt-async')
+var nacl = require('tweetnacl')
+nacl.util = require('tweetnacl-util')
 
-},{}],2:[function(require,module,exports){
+// Code from https://github.com/kaepora/miniLock/blob/master/src/js/miniLock.js
+
+// Input: User key hash (Uint8Array), Salt (Uint8Array), callback function
+// Result: Calls scrypt which returns 32 bytes of key material in an Array,
+// which is then passed to the callback.
+function getScryptKey (key, salt, callback) {
+  scrypt(key, salt, 17, 8, 32, 1000, function (keyBytes) {
+    return callback(keyBytes)
+  }, null)
+}
+
+module.exports = function session (email, password, callback) {
+  var keyHash = new BLAKE2s(nacl.box.secretKeyLength)
+  keyHash.update(nacl.util.decodeUTF8(password))
+
+  getScryptKey(keyHash.digest(), nacl.util.decodeUTF8(email), function (keyBytes) {
+    try {
+      var keyBytesUint8 = new Uint8Array(keyBytes)
+
+      // Generate a keyPair for encryption from the scrypt bytes
+      var keyPair = nacl.box.keyPair.fromSecretKey(keyBytesUint8)
+
+      // Hash the derived keyBytes for use as the 32 Byte seed for
+      // an additional keypair used for signing. This signing
+      // key pair will also be deterministically generated.
+      var signingKeyHash = new BLAKE2s(nacl.sign.seedLength)
+      signingKeyHash.update(keyBytesUint8)
+      var signingKeyPair = nacl.sign.keyPair.fromSeed(signingKeyHash.digest())
+
+      // merge the signing keys into the keyPair
+      keyPair.publicSigningKey = signingKeyPair.publicKey
+      keyPair.secretSigningKey = signingKeyPair.secretKey
+
+      return callback(null, keyPair)
+    } catch (err) {
+      return callback(err)
+    }
+  })
+}
+
+},{"blake2s-js":2,"scrypt-async":4,"tweetnacl":6,"tweetnacl-util":5}],2:[function(require,module,exports){
 var BLAKE2s = (function() {
 
   var MAX_DIGEST_LENGTH = 32;
@@ -1339,6 +1383,8 @@ var BLAKE2s = (function() {
 if (typeof module !== 'undefined' && module.exports) module.exports = BLAKE2s;
 
 },{}],3:[function(require,module,exports){
+
+},{}],4:[function(require,module,exports){
 /*!
  * Fast "async" scrypt implementation in JavaScript.
  * Copyright (c) 2013-2015 Dmitry Chestnykh | BSD License
@@ -1820,7 +1866,7 @@ function scrypt(password, salt, logN, r, dkLen, interruptStep, callback, encodin
 
 if (typeof module !== 'undefined') module.exports = scrypt;
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 (function (Buffer){
 // Written in 2014-2016 by Dmitry Chestnykh and Devi Mandiri.
 // Public domain.
@@ -1874,7 +1920,7 @@ if (typeof module !== 'undefined') module.exports = scrypt;
 }));
 
 }).call(this,require("buffer").Buffer)
-},{"buffer":1}],5:[function(require,module,exports){
+},{"buffer":3}],6:[function(require,module,exports){
 (function(nacl) {
 'use strict';
 
@@ -4264,41 +4310,4 @@ nacl.setPRNG = function(fn) {
 
 })(typeof module !== 'undefined' && module.exports ? module.exports : (self.nacl = self.nacl || {}));
 
-},{"crypto":1}],6:[function(require,module,exports){
-var BLAKE2s = require('blake2s-js')
-var scrypt = require('scrypt-async')
-var nacl = require('tweetnacl')
-nacl.util = require('tweetnacl-util')
-
-// Code from https://github.com/kaepora/miniLock
-// src/js/miniLock.js
-
-// Input: User key hash (Uint8Array), Salt (Uint8Array), callback function
-// Result: Calls scrypt which returns
-//	32 bytes of key material in a Uint8Array,
-//	which then passed to the callback.
-function getScryptKey(key, salt, callback) {
-	scrypt(key, salt, 17, 8, 32, 1000, function(keyBytes) {
-		return callback(nacl.util.decodeBase64(keyBytes))
-	}, 'base64');
-}
-
-module.exports = function session(email, password, callback) {
-  var keyHash = new BLAKE2s(nacl.box.secretKeyLength)
-
-  keyHash.update(nacl.util.decodeUTF8(password))
-
-  var key = keyHash.digest()
-  var salt = nacl.util.decodeUTF8(email)
-
-  getScryptKey(key, salt, function(keyBytes) {
-    try {
-      var keyPair = nacl.box.keyPair.fromSecretKey(keyBytes)
-      callback(null, keyPair)
-    } catch(err) {
-      callback(err)
-    }
-  })
-}
-
-},{"blake2s-js":2,"scrypt-async":3,"tweetnacl":5,"tweetnacl-util":4}]},{},[6]);
+},{"crypto":3}]},{},[1]);
